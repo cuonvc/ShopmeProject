0a5670b7fdb9833c7bad11973ce82356f225de30
Cuong Nguyen <cuongnvc5802@gmail.com>
2022-04-20 23:27:30 +0700

    Refactor code #1

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/shopmebackend/admin/user/controller/UserController.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/shopmebackend/admin/user/controller/UserController.java
@@ -108,8 +108,14 @@ public class UserController {
 
         }
 
-        redirectAttributes.addFlashAttribute("message", "saved user thành công. .");
-        return "redirect:/users";
+        redirectAttributes.addFlashAttribute("message", "The user has been saved successfully.");
+
+        return getRedirectURLtoAffectedUser(user);
+    }
+
+    private String getRedirectURLtoAffectedUser(User user) {
+        String firstPartOfEmail = user.getEmail().split("@")[0];
+        return "redirect:/users/page/1?sortField=id&sortDir=asc&keyword=" + firstPartOfEmail;
     }
 
     @GetMapping("/users/edit/{id}")
